5b69fe57c0e61935116b55108c1805aa83823396
Marco Visani <92217074+mvisani@users.noreply.github.com>
2025-04-14 10:05:47 +0200

    updated some wrongly written functions in Molecules class

--- molecules.py
+++ molecules.py
@@ -1,3 +1,4 @@
+import logging
 from dataclasses import dataclass, field
 from typing import List, Optional, Union
 
@@ -12,13 +13,18 @@ from rdkit.Chem import Mol
 @dataclass
 class Molecule(Hashable):
     apex_cas_number: str
+    failed_cas_number: bool = field(init=False, default=False)
     apex_item_name: str
+    failed_item_name: bool = field(init=False, default=False)
     apex_molecular_weight: float
     apex_molecular_formula: str
     apex_smiles: Optional[str]
-    _apex_cleaned_smiles: Optional[str] = field(init=False, default=None)
-    _rdkit_molecule: Optional[Mol] = field(init=False, default=None)
-    _pubchem_compound: Optional[Union[List[pcp.Compound], pcp.Compound]] = field(
+    failed_smiles: bool = field(init=False, default=False)
+    apex_cleaned_smiles: Optional[str] = field(init=False, default=None)
+    rdkit_molecule: Optional[Mol] = field(init=False, default=None)
+    super_parent_mol: Optional[Mol] = field(init=False, default=None)
+    apex_valid_smiles: bool = field(init=False, default=False)
+    pubchem_compound: Optional[Union[List[pcp.Compound], pcp.Compound]] = field(
         init=False, default=None
     )
 
@@ -27,18 +33,19 @@ class Molecule(Hashable):
             self.apex_smiles = None
         if isinstance(self.apex_smiles, float):
             self.apex_smiles = None
-        self._set_rdkit_molecule()
+
         self._fetch_pubchem_compound()
+        self._set_rdkit_molecule()
         self._clean_apex_smiles()
 
     def _clean_apex_smiles(self):
         """
         Cleans the SMILES string using the molvs library.
         """
-        if self.apex_smiles and self._rdkit_molecule:
+        if self.apex_smiles and self.rdkit_molecule:
             standardizer = Standardizer()
-            cleaned_mol = standardizer.super_parent(self._rdkit_molecule)
-            self._apex_cleaned_smiles = Chem.MolToSmiles(cleaned_mol, canonical=True)
+            self.super_parent_mol = standardizer.super_parent(self.rdkit_molecule)
+            self.apex_cleaned_smiles = Chem.MolToSmiles(self.super_parent_mol)
             return
 
         return
@@ -54,19 +61,32 @@ class Molecule(Hashable):
         """
         Returns the RDKit molecule object.
         """
-        if self._rdkit_molecule is None:
-            if self.apex_smiles is not None and isinstance(self.apex_smiles, str):
+        if not self.rdkit_molecule:
+            try:
+                self.rdkit_molecule = Chem.MolFromSmiles(self.apex_smiles)
+                # since rdkit return None if the SMILES is not valid, we need to
+                # check if the molecule is valid
+                if self.rdkit_molecule:
+                    self.apex_valid_smiles = True
+                    return
+
+                self.rdkit_molecule = Chem.MolFromSmiles(
+                    self.pubchem_compound.canonical_smiles
+                )
+
+            except:
                 try:
-                    self._rdkit_molecule = Chem.MolFromSmiles(self.apex_smiles)
+                    self.rdkit_molecule = Chem.MolFromSmiles(
+                        self.pubchem_compound.canonical_smiles
+                    )
                 except:
-                    print(f"Error parsing SMILES: {self.apex_smiles}")
-                    self._rdkit_molecule = None
+                    self.rdkit_molecule = None
 
     def get_rdkit_molecule(self) -> Optional[Mol]:
         """
         Returns the RDKit molecule object.
         """
-        return self._rdkit_molecule
+        return self.rdkit_molecule
 
     def get_apex_molecular_formula_clean(self) -> str:
         """
@@ -78,81 +98,104 @@ class Molecule(Hashable):
         return self.apex_molecular_weight
 
     def has_compound(self):
-        return self._pubchem_compound is not None
+        """
+        Returns True if at least one compound was found in PubChem.
+        """
+        return self.pubchem_compound is not None
 
     def has_many_compounds(self):
-        if isinstance(self._pubchem_compound, pcp.Compound):
+        if isinstance(self.pubchem_compound, pcp.Compound):
             return False
-        if self._pubchem_compound is None:
+        if self.pubchem_compound is None:
             return False
-        return len(self._pubchem_compound) > 1
+        return len(self.pubchem_compound) > 1
 
     def get_clean_apex_smiles(self) -> Optional[str]:
         """
         Returns the cleaned SMILES string.
         """
-        if self._apex_cleaned_smiles is None:
-            return None
-        return self._apex_cleaned_smiles
+        return self.apex_cleaned_smiles
 
     def _fetch_pubchem_compound(self):
         """
         Fetches the PubChem compound using the CAS number.
         """
-        if self._pubchem_compound is not None:
-            return
 
         # we need to iterate over the CAS number, the item name and the SMILES
         # to find the compound
         # first try the CAS number
-        try:
-            self._pubchem_compound = self._get_compound_from_pubchem(
-                self.apex_cas_number, "name"
+        self.pubchem_compound = self._get_compound_from_pubchem(
+            self.apex_cas_number, "name"
+        )
+        if not self.pubchem_compound:
+            self.failed_cas_number = True
+            # if that fails, try the item name
+            self.pubchem_compound = self._get_compound_from_pubchem(
+                self.apex_item_name, "name"
             )
-        except KeyError:
-            print(f"Error fetching compound by CAS number. Will try with Item Name")
-            try:
-                # if that fails, try the item name
-                self._pubchem_compound = self._get_compound_from_pubchem(
-                    self.apex_cas_number, "name"
+
+        if not self.pubchem_compound:
+            self.failed_item_name = True
+            # if that fails, try the SMILES
+            if self.apex_smiles:
+                self.pubchem_compound = self._get_compound_from_pubchem(
+                    self.apex_smiles, "smiles"
                 )
-            except KeyError:
-                print(f"Error fetching compound by item name.")
-                try:
-                    # if that fails, try the SMILES
-                    self._pubchem_compound = self._get_compound_from_pubchem(
-                        self.apex_smiles, "smiles"
-                    )
-                except ValueError:
-                    print(f"Error fetching compound by SMILES.")
-                    self._pubchem_compound = None
-                    return
+        if not self.pubchem_compound:
+            self.failed_smiles = True
+            print(f"Error fetching compound by SMILES.")
+            self.pubchem_compound = None
+            return
 
         # now we only keep the molecules that have a different inchikey 2D
         compounds = []
         inchikeys = set()
-        for compound in self._pubchem_compound:
+        for compound in self.pubchem_compound:
             if compound.inchikey[:14] not in inchikeys:
                 compounds.append(compound)
                 inchikeys.add(compound.inchikey[:14])
 
         if len(compounds) == 1:
-            self._pubchem_compound = compounds[0]
+            self.pubchem_compound = compounds[0]
         else:
-            self._pubchem_compound = compounds
+            self.pubchem_compound = compounds
 
     def get_pubchem_inchikey(self) -> Optional[Union[str, List[str]]]:
         """
         Returns the InChIKey of the Pubchem compound.
         """
-        if self._pubchem_compound is None:
+        if not self.pubchem_compound:
             return None
         if self.has_many_compounds():
-            return [compound.inchikey for compound in self._pubchem_compound]
+            return [compound.inchikey for compound in self.pubchem_compound]
         else:
-            return self._pubchem_compound.inchikey
+            return self.pubchem_compound.inchikey
 
-    def consistent_hash(self, use_approximation=False):
+    def matching_mass(self) -> bool:
+        """
+        Returns True if the mass of the molecule in the Apex Bio file
+        is matching the mass of the Pubchem compound.
+        """
+        if not self.pubchem_compound:
+            return False
+        if isinstance(self.pubchem_compound, pcp.Compound):
+            return (
+                abs(
+                    float(self.pubchem_compound.molecular_weight)
+                    - self.apex_molecular_weight
+                )
+                < 0.2
+            )
+        else:
+            for compound in self.pubchem_compound:
+                if (
+                    abs(float(compound.molecular_weight) - self.apex_molecular_weight)
+                    < 0.2
+                ):
+                    return True
+        return False
+
+    def consistent_hash(self, use_approximation=True) -> str:
         return sha256(
             {
                 "apex_cas_number": self.apex_cas_number,
@@ -160,27 +203,3 @@ class Molecule(Hashable):
             },
             use_approximation=use_approximation,
         )
-
-    def matching_mass(self) -> bool:
-        """
-        Returns True if the mass of the molecule in the Apex Bio file
-        is matching the mass of the Pubchem compound.
-        """
-        if self._pubchem_compound is None:
-            return False
-        if isinstance(self._pubchem_compound, pcp.Compound):
-            return (
-                abs(
-                    float(self._pubchem_compound.molecular_weight)
-                    - self.apex_molecular_weight
-                )
-                < 0.2
-            )
-        else:
-            for compound in self._pubchem_compound:
-                if (
-                    abs(float(compound.molecular_weight) - self.apex_molecular_weight)
-                    < 0.2
-                ):
-                    return True
-        return False
