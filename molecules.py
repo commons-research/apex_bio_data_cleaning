ea83cff4ccffbffd52d2841dfbbef2776016bacb
Marco Visani <92217074+mvisani@users.noreply.github.com>
2025-04-14 10:56:04 +0200

    added position of sample in 96 well plates

--- molecules.py
+++ molecules.py
@@ -1,13 +1,16 @@
 import logging
-from dataclasses import dataclass, field
+from dataclasses import InitVar, dataclass, field
 from typing import List, Optional, Union
 
+import pandas as pd
 import pubchempy as pcp
 from cache_decorator import Cache
 from dict_hash import Hashable, sha256
 from molvs import Standardizer
 from rdkit import Chem
 from rdkit.Chem import Mol
+from rdkit.Chem.Descriptors import ExactMolWt, MolWt
+from rdkit.Chem.rdMolDescriptors import CalcMolFormula
 
 
 @dataclass
@@ -20,15 +23,19 @@ class Molecule(Hashable):
     apex_molecular_formula: str
     apex_smiles: Optional[str]
     failed_smiles: bool = field(init=False, default=False)
-    apex_cleaned_smiles: Optional[str] = field(init=False, default=None)
+    super_parent_smiles: Optional[str] = field(init=False, default=None)
     rdkit_molecule: Optional[Mol] = field(init=False, default=None)
     super_parent_mol: Optional[Mol] = field(init=False, default=None)
     apex_valid_smiles: bool = field(init=False, default=False)
     pubchem_compound: Optional[Union[List[pcp.Compound], pcp.Compound]] = field(
         init=False, default=None
     )
+    catalog_number: str
+    plate_location: str
+    rack_number_as_str: InitVar[str]
+    rack_number: int = field(init=False)
 
-    def __post_init__(self):
+    def __post_init__(self, rack_number_as_str: str):
         if isinstance(self.apex_smiles, int):
             self.apex_smiles = None
         if isinstance(self.apex_smiles, float):
@@ -38,14 +45,16 @@ class Molecule(Hashable):
         self._set_rdkit_molecule()
         self._clean_apex_smiles()
 
+        self.rack_number = int(rack_number_as_str.split("-")[1])
+
     def _clean_apex_smiles(self):
         """
         Cleans the SMILES string using the molvs library.
         """
-        if self.apex_smiles and self.rdkit_molecule:
+        if self.rdkit_molecule:
             standardizer = Standardizer()
             self.super_parent_mol = standardizer.super_parent(self.rdkit_molecule)
-            self.apex_cleaned_smiles = Chem.MolToSmiles(self.super_parent_mol)
+            self.super_parent_smiles = Chem.MolToSmiles(self.super_parent_mol)
             return
 
         return
@@ -110,11 +119,11 @@ class Molecule(Hashable):
             return False
         return len(self.pubchem_compound) > 1
 
-    def get_clean_apex_smiles(self) -> Optional[str]:
+    def get_super_parent_smiles(self) -> Optional[str]:
         """
         Returns the cleaned SMILES string.
         """
-        return self.apex_cleaned_smiles
+        return self.super_parent_smiles
 
     def _fetch_pubchem_compound(self):
         """
@@ -194,6 +203,40 @@ class Molecule(Hashable):
                 ):
                     return True
         return False
+
+    def to_dataframe(self) -> pd.DataFrame:
+        """
+        Returns the molecule as a pandas dataframe with some metadata.
+        """
+
+        super_parent_exact_mass = (
+            ExactMolWt(self.super_parent_mol) if self.super_parent_mol else None
+        )
+        super_parent_molecular_weight = (
+            MolWt(self.super_parent_mol) if self.super_parent_mol else None
+        )
+        super_parent_formula = (
+            CalcMolFormula(self.super_parent_mol) if self.super_parent_mol else None
+        )
+
+        data = {
+            "catalog_number": self.catalog_number,
+            "plate_location": self.plate_location,
+            "rack_number": self.rack_number,
+            "apex_cas_number": self.apex_cas_number,
+            "apex_item_name": self.apex_item_name,
+            "apex_molecular_weight": self.apex_molecular_weight,
+            "apex_molecular_formula": self.apex_molecular_formula,
+            "apex_smiles": self.apex_smiles,
+            "failed_cas_number": self.failed_cas_number,
+            "failed_item_name": self.failed_item_name,
+            "failed_smiles": self.failed_smiles,
+            "super_parent_smiles": self.super_parent_smiles,
+            "super_parent_exact_mass": super_parent_exact_mass,
+            "super_parent_molecular_weight": super_parent_molecular_weight,
+            "super_parent_molecular_formula": super_parent_formula,
+        }
+        return pd.DataFrame(data, index=[0])
 
     def consistent_hash(self, use_approximation=True) -> str:
         return sha256(
