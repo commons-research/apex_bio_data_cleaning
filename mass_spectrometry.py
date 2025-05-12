36caa2f36394e8ebb9d168511b7d2b06688cda87
Jade Dandois <jade.dandois@bluewin.ch>
2025-05-12 13:22:46 +0200

    commit mass spec

--- mass_spectrometry.py
+++ mass_spectrometry.py
@@ -1,4 +1,4 @@
-# Import some tools 
+# %%
 import pandas as pd
 from molecules import Molecule
 from tqdm.auto import tqdm
@@ -6,9 +6,10 @@ from typing import List
 from datetime import datetime
 from datetime import date 
 
-# read excel
+# %%
 df = pd.read_excel("./apex_bio.xlsx", sheet_name="Chemical Data")
 
+# %%
 from cache_decorator import Cache
 
 @Cache()
@@ -27,12 +28,15 @@ def generate_molecules(df):
         )
         molecules.append(mol)
     return molecules
- 
+
+# %%
 molecules = generate_molecules(df)
 
+# %%
 list_mol = [i.to_dataframe() for i in molecules]
 dataframe_mol = pd.concat(list_mol, ignore_index=True)
 
+# %%
 from datetime import datetime
 import os
 import pandas as pd
@@ -44,9 +48,8 @@ def generate_and_save(df, initials="JDAN", ionisation="both"):
     current_time = datetime.now()
     date = f"{current_time.year:04}{current_time.month:02}{current_time.day:02}"
 
-    for i in range(len(df)):
-        row = df.iloc[[i]]
-        cas_number = row["apex_cas_number"].values[0].replace("-", "_").split(",")[0]
+    for index, row in df.iterrows():  
+        cas_number = row["apex_cas_number"].replace("-", "_").split(",")[0]
 
         # Générer noms de fichiers
         filenames = []
@@ -62,9 +65,9 @@ def generate_and_save(df, initials="JDAN", ionisation="both"):
 
         # Créer le DataFrame CSV
         df_csv = pd.DataFrame(columns=["name", "neutral mass", "formula", "rt"])
-        df_csv.loc[0, "name"] = row["apex_item_name"].values[0]
-        df_csv.loc[0, "neutral mass"] = row["super_parent_exact_mass"].values[0]
-        df_csv.loc[0, "formula"] = row["super_parent_molecular_formula"].values[0]
+        df_csv.loc[0, "name"] = row["apex_item_name"]
+        df_csv.loc[0, "neutral mass"] = row["super_parent_exact_mass"]
+        df_csv.loc[0, "formula"] = row["super_parent_molecular_formula"]
         df_csv.loc[0, "rt"] = 0
         csv_name = f"features_molecule.csv"
 
@@ -76,5 +79,48 @@ def generate_and_save(df, initials="JDAN", ionisation="both"):
 
 generate_and_save(dataframe_mol, initials="JDAN", ionisation="both")
 
+
+# %%
+plate_3 = dataframe_mol[dataframe_mol['rack_number'] == 3]
+
+def generate_csv_mass_spec_pos(df, initials= "JDAN"): 
+    current_time = datetime.now()
+    date = f"{current_time.year:04}{current_time.month:02}{current_time.day:02}"
+
+    csv_mass_spectrometry = pd.DataFrame(columns=["File Name", "Position"])
+    for index, row in df.iterrows():  
+        cas_number = row["apex_cas_number"].replace("-", "_").split(",")[0]
+        location = row["plate_location"]
+        location = location[0] + str(int(location[1:]))
+        position = f"B:{location}"
+        file_name = f"{date}_{initials}_{cas_number}_pos"
+
+        csv_mass_spectrometry.loc[len(csv_mass_spectrometry)] = [file_name, position]
+
+        csv_mass_spectrometry.to_csv("csv_ms_name_pos.csv", index=False)
+
+generate_csv_mass_spec_pos(df=plate_3)
+
+
+# %%
+plate_3 = dataframe_mol[dataframe_mol['rack_number'] == 3]
+
+def generate_csv_mass_spec_neg(df, initials= "JDAN"): 
+    current_time = datetime.now()
+    date = f"{current_time.year:04}{current_time.month:02}{current_time.day:02}"
+
+    csv_mass_spectrometry = pd.DataFrame(columns=["File Name", "Position"])
+    for index, row in df.iterrows():  
+        cas_number = row["apex_cas_number"].replace("-", "_").split(",")[0]
+        location = row["plate_location"]
+        location = location[0] + str(int(location[1:]))
+        position = f"B:{location}"
+        file_name = f"{date}_{initials}_{cas_number}_neg"
+
+        csv_mass_spectrometry.loc[len(csv_mass_spectrometry)] = [file_name, position]
+
+        csv_mass_spectrometry.to_csv("csv_ms_name_neg.csv", index=False)
+
+generate_csv_mass_spec_neg(df=plate_3)
 
 
